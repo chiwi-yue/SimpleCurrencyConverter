05ed314449da3d8a24fc30d681ab76b9e059d9e5
Chiwi <stanmma@gmail.com>
2022-10-08 13:13:24 +1000

    finished

--- simpleCurrencyConverter_stage2.js
+++ simpleCurrencyConverter_stage2.js
@@ -1,7 +1,10 @@
 // Stage 2/4: Convert a currency
+const input = require('sync-input');
+
+const convertCurrencyList = new Array('JPY', 'EUR', 'RUB', 'USD', 'GBP');
 
 class currencyConverterUSD {
-    constructor(quantity) {
+    constructor(quantity = 1) {
         this.USD = 1 * quantity;
         this.JPY = 113.5 * quantity;
         this.EUR = 0.89 * quantity;
@@ -10,10 +13,30 @@ class currencyConverterUSD {
     }
 }
 
-function currencyConverter (convertedCurrency, quantity) {
+function currencyConverter() {
     console.log(`Welcome to Currency Converter!`);
-    let converter = new currencyConverterUSD(quantity);
-    for (const [key, value] of Object.entries(converter)) {
-        console.log(`${quantity} ${convertedCurrency} equals ${value} ${key}`);
+    let initialiser = new currencyConverterUSD();
+    for (const [key, value] of Object.entries(initialiser)) {
+        console.log(`1 USD equals ${value} ${key}`);
     }
+    console.log("I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP");
+    let currencyConvertFrom = input("Type the currency you wish to convert: ").toUpperCase();
+    let currencyConvertTo = input("To: ").toUpperCase();
+    if (!convertCurrencyList.includes(currencyConvertTo)) {
+        console.log("Unknown currency");
+        return;
+    }
+    let amount = input("Amount: ");
+    if (amount < 1) {
+        console.log("The amount cannot be less than 1");
+        return;
+    } else if (isNaN(amount)) {
+        console.log("The amount has to be a number");
+        return;
+    }
+    let result = new currencyConverterUSD(amount);
+    console.log(`Result: ${amount} ${currencyConvertFrom} equals ${result[currencyConvertTo].toFixed(4)} ${currencyConvertTo}`);
 }
+
+
+currencyConverter();
