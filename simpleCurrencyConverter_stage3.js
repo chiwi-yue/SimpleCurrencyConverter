8388fb9a4b26e0520eed931269f7065a8bb51db9
Chiwi <stanmma@gmail.com>
2022-10-09 10:19:53 +1000

    stage3

---
+++ simpleCurrencyConverter_stage3.js
@@ -0,0 +1,50 @@
+// Stage 2/4: Stage 3/4: Convert all currencies
+const input = require('sync-input');
+
+const convertCurrencyList = new Array('JPY', 'EUR', 'RUB', 'USD', 'GBP');
+
+const convertRate = {
+    "USD": 1,
+    "JPY": 113.5,
+    "EUR": 0.89,
+    "RUB": 74.36,
+    "GBP": 0.75,
+};
+
+function currencyConverter() {
+    console.log(`Welcome to Currency Converter!`);
+
+    for (const [key, value] of Object.entries(convertRate)) {
+        console.log(`1 USD equals ${value} ${key}`);
+    }
+
+    let currencyConvertFrom = input("What do you want to convert? \nFrom: ").toUpperCase();
+
+    let currencyConvertTo = input("To: ").toUpperCase();
+    if (!convertCurrencyList.includes(currencyConvertTo)) {
+        console.log("Unknown currency");
+        return;
+    }
+
+    let amount = input("Amount: ");
+
+    if (amount < 1) {
+        console.log("The amount cannot be less than 1");
+        return;
+    } else if (isNaN(amount)) {
+        console.log("The amount has to be a number");
+        return;
+    }
+    
+    let result = getConverteredResult(currencyConvertFrom, currencyConvertTo, amount);
+
+    console.log(`Result: ${amount} ${currencyConvertFrom} equals ${result.toFixed(4)} ${currencyConvertTo}`);
+}
+
+function getConverteredResult(currencyConvertFrom, currencyConvertTo, amount) {
+    let convertedToUSD = amount / convertRate[currencyConvertFrom];
+    let coverteredResult = convertedToUSD * convertRate[currencyConvertTo];
+    return coverteredResult;
+}
+
+currencyConverter();
